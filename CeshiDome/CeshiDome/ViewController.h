224f086de96a9dda441857741df8d8c88e66f29a
luojing52021 <luojing@luojingdeMacBook-Air.local>
2015-05-20 14:59:21 +0800

    321

---
+++ CeshiDome/CeshiDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CeshiDome
+//
+//  Created by 罗静QQ:26843486 on 15/5/20.
+//  Copyright (c) 2015年 罗静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CeshiDome/CeshiDome/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CeshiDome
+//
+//  Created by 罗静QQ:26843486 on 15/5/20.
+//  Copyright (c) 2015年 罗静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
